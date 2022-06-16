b0580afc0ccc7046c9bd3fb5ce1c17a9c3233c1f
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-16 19:39:06 +0100

    do not validate a Cpf with invalid format

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -2,7 +2,18 @@ export default class Cpf {
   readonly value: string;
 
   constructor(value: string) {
+    if (!this.validate(value)) throw new Error('Invalid Cpf');
     this.value = this.removeSymbols(value);
+  }
+
+  private validate(cpf: string) {
+    if (!this.hasValidFormat(cpf)) return false;
+    return true;
+  }
+
+  private hasValidFormat(cpf: string) {
+    const cpfRegex = /^(\d{11}|\d{3}\.\d{3}\.\d{3}-\d{2})$/;
+    return cpfRegex.exec(cpf) !== null;
   }
 
   private removeSymbols(cpf: string) {
--- test/Cpf.test.ts
+++ test/Cpf.test.ts
@@ -10,4 +10,10 @@ describe('Cpf', () => {
     const cpf = new Cpf('669.314.740-22');
     expect(cpf.value).toBe('66931474022');
   });
+
+  it('should not validate a Cpf with invalid format', () => {
+    expect(() => new Cpf('669.314.740.22')).toThrow(new Error('Invalid Cpf'));
+    expect(() => new Cpf(' 669.314.740-22')).toThrow(new Error('Invalid Cpf'));
+    expect(() => new Cpf('669.314.740-22 ')).toThrow(new Error('Invalid Cpf'));
+  });
 });
