74f5323649fd02a40767b380e619e5f55b761837
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-20 22:16:05 +0100

    create an Order with three Items

--- src/Order.ts
+++ src/Order.ts
@@ -1,9 +1,21 @@
 import Cpf from './Cpf';
+import Item from './Item';
+import OrderItem from './OrderItem';
 
 export default class Order {
   private readonly cpf: Cpf;
+  private readonly orderItems: OrderItem[];
 
   constructor(cpf: string) {
     this.cpf = new Cpf(cpf);
+    this.orderItems = [];
+  }
+
+  addItem(item: Item, quantity: number) {
+    this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+  }
+
+  getTotal() {
+    return this.orderItems.reduce((total, orderItem) => total + orderItem.getTotal(), 0);
   }
 }
--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,7 +1,17 @@
+import Item from '../src/Item';
 import Order from '../src/Order';
 
 describe('Order', () => {
   it('should not create an Order with an invalid Cpf', () => {
     expect(() => new Order('669.314.740-21')).toThrow(new Error('Invalid Cpf'));
+  });
+
+  it('should create an Order with three Items', () => {
+    const order = new Order('669.314.740-22');
+    order.addItem(new Item(1, 'Guitarra', 1000), 1);
+    order.addItem(new Item(2, 'Amplificador', 5000), 1);
+    order.addItem(new Item(3, 'Cabo', 30), 3);
+    const total = order.getTotal();
+    expect(total).toBe(6090);
   });
 });
