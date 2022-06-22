73ccdd22a9bc29dcb740efb5b5b6877fc6da287f
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-22 21:19:23 +0100

    calculate the volume of a Dimension

---
+++ test/Dimension.test.ts
@@ -0,0 +1,9 @@
+import Dimension from '../src/Dimension';
+
+describe('Dimension', () => {
+  it('should calculate the volume', () => {
+    const dimension = new Dimension(100, 30, 10);
+    const volume = dimension.getVolume();
+    expect(volume).toBe(0.03);
+  });
+});
