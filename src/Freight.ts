47ab3335b4399ac4b88e632ebe9de942c7ec54e2
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-22 21:52:05 +0100

    calculate the Freight

---
+++ src/Freight.ts
@@ -0,0 +1,19 @@
+import Item from './Item';
+
+export default class Freight {
+  private total = 0;
+  private readonly DISTANCE = 1000;
+  private readonly FACTOR = 100;
+
+  addItem(item: Item, quantity: number) {
+    const volume = item.getVolume();
+    const density = item.getDensity();
+    if (volume && density) {
+      this.total += (quantity * (this.DISTANCE * volume * density)) / this.FACTOR;
+    }
+  }
+
+  getTotal() {
+    return this.total;
+  }
+}
