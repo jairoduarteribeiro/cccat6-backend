988a53b6db8758edabc9bd871a0ef3a11001be3a
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-26 14:08:13 +0100

    refactor GetItems and PlaceOrder tests

--- test/GetItems.test.ts
+++ test/GetItems.test.ts
@@ -4,13 +4,23 @@ import Item from '../src/Item';
 import ItemRepositoryMemory from '../src/ItemRepositoryMemory';
 
 describe('GetItems', () => {
-  it('should get all items', async () => {
-    const itemRepository = new ItemRepositoryMemory();
+  const itemRepository = new ItemRepositoryMemory();
+
+  beforeEach(async () => {
     await itemRepository.save(new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3));
     await itemRepository.save(new Item(2, 'Amplificador', 5000, new Dimension(50, 50, 50), 20));
     await itemRepository.save(new Item(3, 'Cabo', 30, new Dimension(10, 10, 10), 1));
+  });
+
+  it('should get all items', async () => {
     const getItems = new GetItems(itemRepository);
     const output = await getItems.execute();
     expect(output).toHaveLength(3);
+  });
+
+  afterEach(async () => {
+    await itemRepository.delete(1);
+    await itemRepository.delete(2);
+    await itemRepository.delete(3);
   });
 });
--- test/PlaceOrder.test.ts
+++ test/PlaceOrder.test.ts
@@ -5,28 +5,32 @@ import OrderRepositoryMemory from '../src/OrderRepositoryMemory';
 import PlaceOrder from '../src/PlaceOrder';
 
 describe('PlaceOrder', () => {
+  const itemRepository = new ItemRepositoryMemory();
+  const orderRepository = new OrderRepositoryMemory();
+
+  beforeEach(async () => {
+    await itemRepository.save(new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3));
+    await itemRepository.save(new Item(2, 'Amplificador', 5000, new Dimension(50, 50, 50), 20));
+    await itemRepository.save(new Item(3, 'Cabo', 30, new Dimension(10, 10, 10), 1));
+  });
+
   it('should place an order', async () => {
-    const itemRepository = new ItemRepositoryMemory();
-    const orderRepository = new OrderRepositoryMemory();
-    const idItem1 = await itemRepository.save(
-      new Item(1, 'Guitarra', 1000, new Dimension(100, 30, 10), 3)
-    );
-    const idItem2 = await itemRepository.save(
-      new Item(2, 'Amplificador', 5000, new Dimension(50, 50, 50), 20)
-    );
-    const idItem3 = await itemRepository.save(
-      new Item(3, 'Cabo', 30, new Dimension(10, 10, 10), 1)
-    );
     const placeOrder = new PlaceOrder(itemRepository, orderRepository);
     const input = {
       cpf: '669.314.740-22',
       orderItems: [
-        { idItem: idItem1, quantity: 1 },
-        { idItem: idItem2, quantity: 1 },
-        { idItem: idItem3, quantity: 3 },
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 3 },
       ],
     };
     const output = await placeOrder.execute(input);
     expect(output.total).toBe(6350);
+  });
+
+  afterEach(async () => {
+    await itemRepository.delete(1);
+    await itemRepository.delete(2);
+    await itemRepository.delete(3);
   });
 });
