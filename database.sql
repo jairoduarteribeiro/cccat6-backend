965fae1519645b7d3dc2d0c75aa308dd2eaa855e
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-26 13:50:11 +0100

    add script to create table item

--- database.sql
+++ database.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE cccat6_test;
 
 CREATE SCHEMA ccca;
+
+CREATE TABLE ccca.item (
+  id_item SERIAL PRIMARY KEY,
+  category TEXT,
+  description TEXT,
+  price NUMERIC,
+  width INTEGER,
+  height INTEGER,
+  length INTEGER,
+  weight INTEGER
+);
