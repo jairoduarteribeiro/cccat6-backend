441926f53e80108bbf1038aba505317ab7b5df81
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-27 19:26:12 +0100

    create OrderCoupon entity

--- src/domain/entity/Order.ts
+++ src/domain/entity/Order.ts
@@ -2,12 +2,13 @@ import Coupon from './Coupon';
 import Cpf from './Cpf';
 import Freight from './Freight';
 import Item from './Item';
+import OrderCoupon from './OrderCoupon';
 import OrderItem from './OrderItem';
 
 export default class Order {
   private readonly cpf: Cpf;
   private readonly orderItems: OrderItem[];
-  private coupon?: Coupon;
+  private coupon?: OrderCoupon;
   private freight = new Freight();
 
   constructor(cpf: string, private readonly issueDate = new Date()) {
@@ -22,13 +23,13 @@ export default class Order {
 
   addCoupon(coupon: Coupon) {
     if (!coupon.isExpired(this.issueDate)) {
-      this.coupon = coupon;
+      this.coupon = new OrderCoupon(coupon.couponCode, coupon.percentage);
     }
   }
 
   getTotal() {
     const total = this.orderItems.reduce((total, orderItem) => total + orderItem.getTotal(), 0);
-    const discount = this.coupon ? (total * this.coupon.percentage) / 100 : 0;
+    const discount = this.coupon?.calculateDiscount(total) ?? 0;
     return total - discount + this.freight.getTotal();
   }
 }
