833a292efb418c779894f9c847339c0a9a06cfdf
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-10 22:46:47 +0100

    get all orders

---
+++ src/application/query/GetOrdersQuery.ts
@@ -0,0 +1,21 @@
+import Connection from '../../infra/database/Connection';
+
+export default class GetOrdersQuery {
+  constructor(private readonly connection: Connection) {}
+
+  async execute(orderCode: string): Promise<Output[]> {
+    const orders: Output[] = [];
+    const ordersData = await this.connection.query('SELECT order_code, total FROM ccca.order', []);
+    for (const orderData of ordersData) {
+      const orderCode = orderData['order_code'];
+      const total = parseFloat(orderData['total']);
+      orders.push({ orderCode, total });
+    }
+    return orders;
+  }
+}
+
+type Output = {
+  orderCode: string;
+  total: number;
+};
