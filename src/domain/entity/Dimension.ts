3e7a05faa7296884115512c46430a018fea68de8
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-13 00:08:05 +0100

    verify if any dimension is negative

--- src/domain/entity/Dimension.ts
+++ src/domain/entity/Dimension.ts
@@ -1,5 +1,14 @@
 export default class Dimension {
-  constructor(readonly width: number, readonly height: number, readonly length: number) {}
+  readonly width: number;
+  readonly height: number;
+  readonly length: number;
+
+  constructor(width: number, height: number, length: number) {
+    if (width < 0 || height < 0 || length < 0) throw new Error('Invalid dimension');
+    this.width = width;
+    this.height = height;
+    this.length = length;
+  }
 
   getVolume() {
     return (this.width / 100) * (this.height / 100) * (this.length / 100);
--- test/unit/Dimension.test.ts
+++ test/unit/Dimension.test.ts
@@ -6,4 +6,10 @@ describe('Dimension', () => {
     const volume = dimension.getVolume();
     expect(volume).toBe(0.03);
   });
+
+  it('should throw an Error if any dimension is negative', () => {
+    expect(() => new Dimension(-1, 30, 10)).toThrow(new Error('Invalid dimension'));
+    expect(() => new Dimension(100, -1, 10)).toThrow(new Error('Invalid dimension'));
+    expect(() => new Dimension(100, 30, -1)).toThrow(new Error('Invalid dimension'));
+  });
 });
